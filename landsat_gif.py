e8edd7b4ddb13100ccc691587eb6d6c7d836fe16
vincentsarago <vincent.sarago@gmail.com>
2016-01-20 00:26:43 -0500

    Creation of command line style using Click Add NDVI Add some prints Add some test Removed Unix 'rm'

--- landsat_gif.py
+++ landsat_gif.py
@@ -1,39 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import click
+
 import os
 import sys
 import datetime
 import urllib2
 import numpy as np
 import requests
+import shutil
 import uuid
 import json
-
+import math
+import time
 from skimage import exposure
 from osgeo import gdal, ogr, osr
 from shapely.wkt import loads
-
-##############################################################
-
-tmp_dir = '/tmp/'
-out_dir = '~/'
-
-###########
-# INPUT
-pr = None
-lat = 45.542167 #Montréal
-lon = -73.626149 #Montréal
-cc = 20
-start_date = '2015-01-01'
-end_date = '2016-01-01'
-
-buff_size = 10000 #in m
-
-##############
-landsat_api_url = 'https://api.remotepixel.ca/landsat'
-#or
-#landsat_api_url = 'https://api.developmentseed.org/landsat'
-##############
 
 ##########################   
 #Form Landsat-util by developmentseed.org (https://github.com/developmentseed/landsat-util)
@@ -125,11 +107,10 @@ def search(quer, limit=200):
         result['status'] = u'SUCCESS'
         result['total'] = r_dict['meta']['results']['total']
         result['results'] = [{'sceneID': i['sceneID'],
-                              'sat_type': u'L8',
                               'path': '{:03d}'.format(int(i['path'])),
                               'row': '{:03d}'.format(int(i['row'])),
-                              'browseURL': i['browseURL'],
                               'date': i['acquisitionDate'],
+                              'dayOrNight': i['dayOrNight'], 
                               'lowerLeftCornerLatitude':i['lowerLeftCornerLatitude'],
                               'lowerLeftCornerLongitude':i['lowerLeftCornerLongitude'],
                               'lowerRightCornerLatitude':i['lowerRightCornerLatitude'],
@@ -144,17 +125,116 @@ def search(quer, limit=200):
     return result
 ##########################   
 
-def worker(_taskid=None):
+##############################################################
+# landsat_extractMTL()
+# Extract Metadata value
+#
+def landsat_extractMTL(meta, param):
+    """ Extract Parameters from MTL file """
+    
+    for line in meta:
+        data = line.split(' = ')
+        if (data[0]).strip() == param:
+            return (data[1]).strip()
+        
+##############################################################
+#Conversion Top Of Atmosphere planetary reflectance
+#REF: http://landsat.usgs.gov/Landsat8_Using_Product.php
+def landsat_dnToReflectance_USGS(dn, nband, mfile):
+    """ Apply correction - DN to TOA value """
+    
+    Mp = float(landsat_extractMTL(mfile, "REFLECTANCE_MULT_BAND_%i" % nband))
+    Ap = float(landsat_extractMTL(mfile, "REFLECTANCE_ADD_BAND_%i" % nband))
+    SE = math.radians(float(landsat_extractMTL(mfile, "SUN_ELEVATION")))
+    Reflect_toa = (np.where(dn > 0, (Mp*dn + Ap) / math.sin(SE), 0))
+    return Reflect_toa
+
+################################################################################
+################################################################################
+################################################################################
+
+##############
+landsat_api_url = 'https://api.remotepixel.ca/landsat'
+#or
+#landsat_api_url = 'https://api.developmentseed.org/landsat'
+##############
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option('--lat', type=float, default=None,
+    help='Latitude of the query, between 90 and -90.')
+@click.option('--lon', type=float, default=None,
+    help='Longitude of the query, between 180 and -180.')
+@click.option(
+    '--path_row', type=str, default=None,
+    help='Paths and Rows in order separated by comma. Use quotes "001,003".')
+
+@click.option(
+    '--cloud', type=float, default=20.,
+    help='Start date of the query in the format YYYYMMDD.')
+@click.option(
+    '--start_date', type=str, default='2015-01-01',
+    help='Start date of the query in the format YYYY-MM-DD.')
+@click.option(
+    '--end_date', type=str, default=time.strftime('%Y-%m-%d'),
+    help='End date of the query in the format YYYY-MM-DD.')
+@click.option(
+    '--buffer', type=int, default=10000,
+    help='Buffer size around lat/lon point for image creation.')
+@click.option(
+    '--taskid', type=str, default=str(uuid.uuid1()),
+    help='UUID of task.')
+@click.option(
+    '--ndvi', is_flag=True,
+    help='Create NDVI animation instead of RGB')
+@click.option(
+    '--aws', is_flag=True,
+    help='If you are running this code on AWS.')
+@click.option(
+    '--path', type=click.Path(exists=True), default='.',
+    help='Set the path where the file will be saved.')
+
+def worker(lat, lon, cloud, path_row, start_date, end_date, buffer, taskid, ndvi, path, aws):
     """ Create animated GIF from landsat 8 data"""
-    if not _taskid:
-        _taskid = str(uuid.uuid1())
+
+    #Test 
+    #lat lon has to be defined if path_row isn't 
+    if (not lat) | (not lon):
+        print "No defined lat-lon"
+        if (not path_row):
+            print "No defined Path-Row for query as well"
+            print "Cannot perform querry, please make sure to include at least lat and lon options"
+            sys.exit(1)
 
     #Query Scenes
-    landsat_query = query_builder(paths_rows=pr, lat=lat, lon=lon, start_date=start_date, end_date=end_date, cloud_max=cc)
-
+    print
+    print "Building Landsat-API request"
+    landsat_query = query_builder(paths_rows=path_row, lat=lat, lon=lon, start_date=start_date, end_date=end_date, cloud_max=cloud)
+    print "Searching Landsat 8 images"
     candidate_scenes = search(landsat_query)
-    im2process = candidate_scenes['results']
 
+
+    if not candidate_scenes.has_key('results'):
+        print "Landsat-API Querry returned with 'Not Found message'"
+        sys.exit(1)
+        
+    im2process = candidate_scenes['results']
+    all_ids = [i['sceneID'] for i in im2process]
+    
+    print '{} Landsat scene found'.format(len(all_ids))
+    print 'landsat ids: {}'.format(", ".join(all_ids))
+
+    #Check Only if ROW is the same (same date) 
+    all_row = [i['row'] for i in im2process]
+    if len(list(set(all_row))) > 1:
+        print '''AOI covering more than one Row : 
+        Please choose one of the following: {}'''.format(' | '.join(list(set(all_row))))
+        sys.exit(1)
+    
+    #Construct AOI  (square in WebMercator)
     wgs = osr.SpatialReference()  
     wgs.ImportFromEPSG(4326)
      
@@ -169,18 +249,13 @@ def worker(_taskid=None):
     
     pt.Transform(wgsTowm)
     shPt = loads(pt.ExportToWkt())
-    polB = shPt.buffer(buff_size, cap_style=3)
+    polB = shPt.buffer(buffer, cap_style=3)
     
     aoi = ogr.CreateGeometryFromWkt(polB.wkt)
     aoi.Transform(wmTowgs) #Transform AOI in WGS84
     pt = pol = polB = None
-
-    #Check if multiple Path Row
     
-    
-    ######
-    
-    #Check if AOI is within Landsat Scene
+    print "Excluding Landsat 8 scene not covering the AOI"
     proc_images = []
     for ii in range(len(im2process)):
         imgMeta = im2process[ii]
@@ -200,11 +275,9 @@ def worker(_taskid=None):
         ring = poly = None
      
     if len(proc_images) == 0:
-        print 'no Image found'  
+        print 'No Image found covering the AOI - try reducing buffer size or changing lat-lon'  
     else:
-        
-        #print proc_images
-        workdir = os.path.join(tmp_dir, _taskid)
+        workdir = os.path.join(path, taskid)
         if not os.path.exists(workdir):
             os.makedirs(workdir, 0775)
 
@@ -213,12 +286,18 @@ def worker(_taskid=None):
         for ii in range(len(proc_images)):
              
             im = proc_images[ii]
+            print 'Processing Landsat image {}'.format(im['sceneID'])
+            
             out_im = os.path.join(workdir, '{}.tif'.format(im['date']))
+            
             try:
                 WRSPath = im['path']
                 WRSRow = im['row']
-                landsat_address = 'http://landsat-pds.s3.amazonaws.com/L8/{path}/{row}/{id}/{id}'.format(path=WRSPath, row=WRSRow, id=im['sceneID'])
-                 
+                if aws:
+                    landsat_address = 's3://landsat-pds/L8/{path}/{row}/{id}/{id}'.format(path=WRSPath, row=WRSRow, id=im['sceneID'])
+                else: 
+                    landsat_address = 'http://landsat-pds.s3.amazonaws.com/L8/{path}/{row}/{id}/{id}'.format(path=WRSPath, row=WRSRow, id=im['sceneID'])
+                                       
                 meta_file = '{0}_MTL.txt'.format(landsat_address)
                 meta_data = urllib2.urlopen(meta_file).readlines()
                 
@@ -240,57 +319,82 @@ def worker(_taskid=None):
                 aoi_bounds = aoi.GetEnvelope()
           
                 x_off = int((aoi_bounds[0] - geoT[0]) / geoT[1])
-                y_off = int((aoi_bounds[3] - geoT[3]) / geoT[5])
+                y_off = int((aoi_bounds[3] - geoT[3]) / geoT[5])             
                 x_size = int(((aoi_bounds[0] - geoT[3]) / geoT[5]) - ((aoi_bounds[1] - geoT[3]) / geoT[5]))
                 y_size = int(((aoi_bounds[2] - geoT[3]) / geoT[5]) - ((aoi_bounds[3] - geoT[3]) / geoT[5]))
-          
+    
                 #Create RGB file
                 ngeo = list(geoT)
                 ngeo[0] = aoi_bounds[0]
                 ngeo[3] = aoi_bounds[3]
-          
-                driver = gdal.GetDriverByName("GTiff")
-                dst_ds = driver.Create(out_im, x_size, y_size, 3, gdal.GDT_Byte)
-                dst_ds.SetGeoTransform(tuple(ngeo))
-                dst_ds.SetProjection(proj)
-                rgb = [4,3,2]
-          
-                for b in range(len(rgb)):
-                    band_address = '/vsicurl/{0}_B{1}.TIF'.format(landsat_address, rgb[b])
-                    awsim = gdal.Open(band_address, gdal.GA_ReadOnly)
-                    #2% color rescale
-                    arr = awsim.GetRasterBand(1).ReadAsArray(x_off, y_off, x_size, y_size) 
-                    p2, p98 = np.percentile(arr[arr > 0], (2, 98))
-                    dst_ds.GetRasterBand(b+1).WriteArray(np.where(arr > 0, exposure.rescale_intensity(arr, in_range=(p2, p98), out_range=(1,255)), 0))
-                    dst_ds.GetRasterBand(b+1).SetNoDataValue(0)
-                    awsim = None
-                      
-                dst_ds = None    # save, close
-  
-                #Convert TIF in JPG and add date
-                str1 = 'convert -font Helvetica -pointsize 30 -fill white -draw "text 20,50 \''
-                str2 = im['date'] + '\'"'
-                out_jpg = out_im.replace('.tif','.jpg')
-                cmd = "%s%s %s %s" % (str1, str2, out_im, out_jpg)
-                os.system(cmd)
-                os.remove(out_im) #delete TIF
+            
+                if ndvi:
+                    driver = gdal.GetDriverByName("GTiff")
+                    dst_ds = driver.Create(out_im, x_size, y_size, 1, gdal.GDT_Float32)
+                    dst_ds.SetGeoTransform(tuple(ngeo))
+                    dst_ds.SetProjection(proj)
+    
+                    band5_address = '/vsicurl/{0}_B5.TIF'.format(landsat_address)
+                    awsim5 = gdal.Open(band5_address, gdal.GA_ReadOnly)
+                    arr5 = awsim5.GetRasterBand(1).ReadAsArray(x_off, y_off, x_size, y_size) 
+                    arr5 = landsat_dnToReflectance_USGS(arr5, 5, meta_data) 
+                    
+                    band4_address = '/vsicurl/{0}_B4.TIF'.format(landsat_address)
+                    awsim4 = gdal.Open(band4_address, gdal.GA_ReadOnly)
+                    arr4 = awsim4.GetRasterBand(1).ReadAsArray(x_off, y_off, x_size, y_size) 
+                    arr4 = landsat_dnToReflectance_USGS(arr4, 4, meta_data)    
+    
+                    dst_ds.GetRasterBand(1).WriteArray(np.where( arr5*arr4 > 0, np.nan_to_num((arr5 - arr4) / (arr5 + arr4)), 0))
+                    dst_ds.GetRasterBand(1).SetNoDataValue(-9999)
+                    ratio = dst_ds = awsim4 = awsim5 = arr4 = arr5 = None
+                    
+                    #This part can be replace in pure python (PIL)
+                    #Convert TIF in JPG and add date
+                    out_jpg = out_im.replace('.tif','.jpg')
+                    os.system('convert -font Helvetica -pointsize 30 -fill white -draw "text 20,50 \'{0}\'" {1} {2}'.format(im['date'], out_im, out_jpg))
+                    os.remove(out_im)              
+                    
+                else:
+                    driver = gdal.GetDriverByName("GTiff")
+                    dst_ds = driver.Create(out_im, x_size, y_size, 3, gdal.GDT_Byte)
+                    dst_ds.SetGeoTransform(tuple(ngeo))
+                    dst_ds.SetProjection(proj)
+                    rgb = [4,3,2]
+                    for b in range(len(rgb)):
+                        band_address = '/vsicurl/{0}_B{1}.TIF'.format(landsat_address, rgb[b])
+                        awsim = gdal.Open(band_address, gdal.GA_ReadOnly)
+                        arr = awsim.GetRasterBand(1).ReadAsArray(x_off, y_off, x_size, y_size)                                       
+                        p2, p98 = np.percentile(arr[arr > 0], (2, 98))
+                        dst_ds.GetRasterBand(b+1).WriteArray(np.where(arr > 0, exposure.rescale_intensity(arr, in_range=(p2, p98), out_range=(1,255)), 0))
+                        dst_ds.GetRasterBand(b+1).SetNoDataValue(0)
+                        awsim = arr = None
+                    dst_ds = None    # save, close
+
+                    #This part can be replace in pure python (PIL)                
+                    #Convert TIF in JPG and add date
+                    out_jpg = out_im.replace('.tif','.jpg')
+                    os.system('convert -font Helvetica -pointsize 30 -fill white -draw "text 20,50 \'{0}\'" {1} {2}'.format(im['date'], out_im, out_jpg))
+                    os.remove(out_im) #delete TIF
                 
                 date_array.append(im['date'])
                 l8_images.append(out_jpg)
             except:
-                print 'failed'
+                print 'Failed to process Landsat image {}'.format(im['sceneID'])
 
-        #Sort image by date
-        sorted_index = np.argsort(date_array)
-        l8sort = [l8_images[i] for i in sorted_index]
-        for i in range(len(l8sort)):
-            os.rename(l8sort[i], os.path.join(workdir, '{:05d}.jpg'.format(i)))
-    
-    #Create GIF
-    gif_file = os.path.join(out_dir, "%s.gif" % _taskid)
-    inJpg = os.path.join(workdir, "*.jpg")
-    os.system('convert -delay 30 -depth 8 -layers optimize -quality 80 -loop 0 {0} {1}'.format(inJpg, gif_file))
-    os.system('rm -rf {}'.format(workdir))
+        if len(date_array) > 0:
+            #Sort image by date and rename with number
+            sorted_index = np.argsort(date_array)
+            l8sort = [l8_images[i] for i in sorted_index]
+            for i in range(len(l8sort)):
+                os.rename(l8sort[i], os.path.join(workdir, '{:05d}.jpg'.format(i)))
+        
+            #This part can be replace in pure python
+            #Create GIF
+            gif_file = os.path.join(path, "%s.gif" % taskid)
+            inJpg = os.path.join(workdir, "*.jpg")
+            os.system('convert -delay 30 -depth 8 -layers optimize -quality 80 -loop 0 {0} {1}'.format(inJpg, gif_file))
+        
+        shutil.rmtree(workdir)
 
 if __name__ == '__main__':
     worker()
